1945c23dd3154aeb74a7d189be02b2112093ff05
ericrodriguesfer <er601432@gmail.com>
2021-01-28 11:41:33 -0300

    arrumando a estrutura do schema do estudo de JPA no lab de PI

--- estudo-projeto-integrado/sistema-bancario-jpa-orm/schema-banco-de-dados/schema.sql
+++ estudo-projeto-integrado/sistema-bancario-jpa-orm/schema-banco-de-dados/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS bancojpa;
 USE bancojpa;
 
 CREATE TABLE IF NOT EXISTS conta (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     matricula VARCHAR(6) NOT NULL,
     nome VARCHAR(40) NOT NULL,
     cpf VARCHAR(14) NOT NULL,
@@ -12,18 +12,18 @@ CREATE TABLE IF NOT EXISTS conta (
 );
 
 CREATE TABLE IF NOT EXISTS operacao (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	matricula_conta VARCHAR(6) NOT NULL,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    matricula_conta VARCHAR(6) NOT NULL,
     email_conta VARCHAR(40) NOT NULL,
     tipo CHAR(1) NOT NULL,
-	valor DOUBLE NOT NULL
+    valor DOUBLE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS transacoes (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	matricula_conta_realizou VARCHAR(6) NOT NULL,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    matricula_conta_realizou VARCHAR(6) NOT NULL,
     email_conta_realizou VARCHAR(40) NOT NULL,
-	matricula_conta_recebeu VARCHAR(6) NOT NULL,
+    matricula_conta_recebeu VARCHAR(6) NOT NULL,
     email_conta_recebeu VARCHAR(40) NOT NULL,
-	valor DOUBLE NOT NULL
+    valor DOUBLE NOT NULL
 );
